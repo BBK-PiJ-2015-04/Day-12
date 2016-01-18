e01ce89e2ed6f0c63a87dda44a8c7ea439542777
HS <none@email.none>
2016-01-18 18:12:28 +0000

    .

---
+++ PersonTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static java.lang.Math.sqrt;
+import static org.junit.Assert.assertEquals;
+
+public class PersonTest {
+
+	@Test
+	public void myTest() {
+		System.out.println("Ouch!");
+		System.out.println(sqrt(2));
+		assertEquals(1, 1);
+	}
+
+}
